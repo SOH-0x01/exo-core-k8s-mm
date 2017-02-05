51c757da4f06a4f12099f81bf4ec5848b823b1f2
datsci-0x01 <datsci.0x01@gmail.com>
2017-02-05 02:11:53 +0100

    Initial version

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+## Usage: .\setup.sh
+## Install basic requirement for the project
+#
+pip install -U ansible
+pip install -U cs
+pip install -U sshpubkeys
+pip install -U boto
+brew install cfssl
+brew install kubectl
